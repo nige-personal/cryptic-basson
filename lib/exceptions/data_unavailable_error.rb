5b4037ffcd6f237e222d051d484b65fcbebaa1fb
nige-personal <nigelsurtees@nigels-mbp.home>
2018-09-17 11:25:00 +0100

    delete

--- lib/exceptions/data_unavailable_error.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DataUnavailableError < StandardError
-  def initialize(msg='Data not available')
-    super
-  end
-end
--- lib/person_dto.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# struct for standardising person data
-class PersonDto
-  attr_accessor :id, :value, :first_name, :last_name, :country
-  attr_accessor :latitude, :longitude, :company, :email, :address
-end
